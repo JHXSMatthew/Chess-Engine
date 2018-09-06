962eb3692ff27f803e953d75813eb48c309c0c9f
Mathew Vo <mathew@andchill.com>
2018-09-06 18:21:00 +1000

    Initial boardrep

--- chessEngine/src/Piece.java
+++ chessEngine/src/Piece.java
@@ -1,3 +1,18 @@
+import java.util.*;
+
 public class Piece {
+
+    public enum Value {
+        whitePawn("p") ,whiteRook ("r"), whiteKnight ("n"), whiteBishop ("b"), whiteQueen ("q"), whiteKing ("k"),
+        blackPawn ("P"), blackRook ("R"), blackKnight ("N"), blackBishop ("B"), blackQueen ("Q"), blackKing ("K");
+
+        String  repStr;
+        Value(String repStr){
+            this.repStr = repStr;
+        }
+    }
+
+
+
 
 }
--- chessEngine/src/Position.java
+++ chessEngine/src/Position.java
@@ -1,3 +1,30 @@
 public class Position {
+    public int[] board = new int[128];
 
+    public int toSquare (int rank, int file) {
+        return 16 * rank + file;
+    }
+
+    public int rank(int square) {
+        return square >> 4;
+    }
+
+    public int toSquare (int index) {
+        return index + (index & ~7);
+    }
+
+    public int toIndex (int square) {
+        return (square + (square & 7)) >> 1;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
