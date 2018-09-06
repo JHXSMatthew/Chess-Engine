6a006a218968a0801ef00d251fd995f89cffffa0
JHXSMatthew <68638023@qq.com>
2018-09-06 18:07:49 +1000

    wip

---
+++ chess-engine-frontend/src/resource/Piece.js
@@ -0,0 +1,25 @@
+import whiteBishop from './whiteBishop.svg'
+import whiteKing from './whiteKing.svg'
+import whiteKnight from './whiteKnight.svg'
+import whitePawn from './whitePawn.svg'
+import whiteQueen from './whiteQueen.svg'
+import whiteRook from './whiteRook.svg'
+
+import blackBishop from './blackBishop.svg'
+import blackKing from './blackKing.svg'
+import blackKnight from './blackKnight.svg'
+import blackPawn from './blackPawn.svg'
+import blackQueen from './blackQueen.svg'
+import blackRook from './blackRook.svg'
+
+
+
+
+
+
+const piecesSvg = {
+  p: whitePawn,
+  P: blackPawn,
+  
+
+}
