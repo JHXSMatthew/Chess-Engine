4fe8c132f1a605a9c61b069d6bdb7352a6be674f
Gary Chen <cpldhycxc@gmail.com>
2018-09-23 19:59:18 +1000

    get put patch post all done

--- api/complete/src/main/java/hello/MoveController.java
+++ api/complete/src/main/java/hello/MoveController.java
@@ -1,7 +1,10 @@
 package hello;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import engine.*;
@@ -53,62 +56,6 @@ public class MoveController {
         return mhr.findAll();
     }
 
-    @Autowired
-    private GameRoomRepository grr;
-    private Integer id = 1;
-    @CrossOrigin(origins = "*")
-    @PostMapping("/game")
-    public GameRoom newGame(){
-        String initState = engine.getInitState();
-        UUID gameId = UUID.randomUUID();
-        GameRoom gr = new GameRoom();
-        gr.setId(id);
-        id += 1;
-        gr.setRoomId(gameId.toString());
-        gr.setNumOfUser(1);
-        gr.setState(initState);
-        gr.setStatus("lobby");
-        grr.save(gr);
-        return gr;
-    }
 
-    @CrossOrigin(origins = "*")
-    @GetMapping("/game/{id}")
-    // Get status and state
-    public GameInfoResponse getGameInfo(@RequestParam UUID id){
-        GameInfoResponse info = new GameInfoResponse();
-        Iterable<GameRoom> result = grr.findAll();
-        for(GameRoom i : result){
-            if(i.getRoomId().equals(id.toString())){
-                info.setState(i.getState());
-                info.setStatus(i.getStatus());
-            }
-        }
-        return info;
-    }
-
-    @CrossOrigin(origins = "*")
-    @PatchMapping("/game/{id}?move={move}")
-    public StateContainer handlePatch(@RequestParam UUID id, @RequestParam String move){
-        Iterable<GameRoom> result = grr.findAll();
-        // Assume move is : "from to"
-        int from = Integer.parseInt(move.split(" ")[0]);
-        int to = Integer.parseInt(move.split(" ")[1]);
-        String resultState = "";
-        for(GameRoom i : result) {
-            if(i.getRoomId().equals(id.toString())){
-                resultState = engine.move(i.getState(),from, to);
-            }
-        }
-        StateContainer finalState = new StateContainer();
-        finalState.setCurentState(resultState);
-        return finalState;
-    }
-
-    @CrossOrigin(origins = "*")
-    @PutMapping("/game/{id}")
-    public String handlePutAction(@RequestParam UUID id){
-        return null;
-    }
 
 }
