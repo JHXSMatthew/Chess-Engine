a41541abe4bb31bd2578f31a42111519a43d0a39
William Guo <williamg2103@gmail.com>
2018-09-25 18:31:58 +1000

    Added sidebar elements when started new local game

--- chess-engine-frontend/src/component/Sidebar.js
+++ chess-engine-frontend/src/component/Sidebar.js
@@ -8,35 +8,53 @@ export default class Sidebar extends React.Component{
 
   render(){
     const {saveGame, loadGame, endGame, newLocalGame, gameType} = this.props;
-
     if (gameType) {
-    return (
-      <div>
-        <button className='btn btn-primary' onClick={saveGame}> Save </button>
-        <button className='btn btn-secondary ml-2' onClick={loadGame}> Load </button>
-        
-        <button className='btn btn-secondary ml-2' data-toggle='modal' data-target='#endGameScreen' 
-                onClick={endGame}> Resign </button>
-      </div>
-    );
+      return (
+        <div className="sidebar">
+          <div className="d-flex flex-row flex-fill">
+            <div className="p-2">Opponent: </div>
+            <div className="p-2">{PiecesSVG['p']}</div>
+          </div>
+          <div className="d-flex flex-row flex-fill">
+            <div className="p-2">Timer: </div>
+            <div className="p-2">5:00/60:00</div>
+          </div>
+          <div className="d-flex flex-row flex-fill">
+            <div className="p-2">{PiecesSVG['P']}</div>
+            <div className="p-2">'s Turn</div>
+          </div>
+          <div className="d-flex flex-row flex-fill">
+            <div className="p-2">Move History: </div>
+          </div>
+          <div className="d-flex flex-row flex-fill">
+            <div className="p-2">
+              <button className='btn btn-secondary ml-2' data-toggle='modal' data-target='#endGameScreen' 
+                  onClick={endGame}> Resign </button>
+            </div>
+            <div className="p-2">
+              <button className='btn btn-primary' onClick={saveGame}> Save </button>
+            </div>
+          </div>
+          <div className="d-flex flex-row flex-fill">
+            <div className="p-2">Timer: </div>
+            <div className="p-2">5:00/60:00</div>
+          </div>
+          <div className="d-flex flex-row flex-fill">
+            <div className="p-2">You: </div>
+            <div className="p-2">{PiecesSVG['P']}</div>
+          </div>
+        </div>
+      );
     } else {
       return (
         <div className="sidebar">
-          <div className="row align-items-end">
-            <div className="col-5">
+          <div className="d-flex flex-row flex-fill align-items-end">
               <button className='btn btn-primary' onClick={newLocalGame}> New Local Game </button>
-            </div>
-            <div className="col-5">
               <button className='btn btn-primary' > New Network Game </button>
-            </div>
           </div>
-          <div className="row align-items-start">
-            <div className="col-5">
+          <div className="d-flex flex-row flex-fill align-items-start">
               <button className='btn btn-primary' > Load Local Game </button>
-            </div>
-            <div className="col-5">
               <button className='btn btn-primary' > Load Network Game </button>
-            </div>
           </div>
         </div>
       );
