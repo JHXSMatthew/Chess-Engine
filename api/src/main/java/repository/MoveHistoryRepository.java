f1f30d24602a40c7cbc6c659658d7f9d7d14e5f6
JHXSMatthew <68638023@qq.com>
2018-09-27 14:11:57 +1000

    move package under app so that no need to manually
    add scan options
    fix id/uuid db column type inconsistency

--- api/src/main/java/repository/MoveHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.move.MoveHistory;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MoveHistoryRepository extends CrudRepository<MoveHistory, Integer> {
-}
