ef5ace0504dfb921e2c6d1a49bd91072a82397d9
JHXSMatthew <68638023@qq.com>
2018-10-14 16:27:25 +1100

    update sql statement

--- api/src/main/java/app/repository/MoveHistoryRepository.java
+++ api/src/main/java/app/repository/MoveHistoryRepository.java
@@ -11,10 +11,10 @@ import java.util.Optional;
 @Repository
 public interface MoveHistoryRepository extends CrudRepository<MoveHistory, Integer> {
 
-    @Query(value = "SELECT * FROM move_history WHERE game_id= ?1 ORDER BY id DESC LIMIT 1;", nativeQuery=true)
+    @Query(value = "SELECT * FROM move_history_new WHERE game_id= ?1 ORDER BY id DESC LIMIT 1;", nativeQuery=true)
     Optional<MoveHistory> findLastHistoryByGameId(String gameId);
 
-    @Query( value = "SELECT * FROM move_history WHERE game_id= ?1", nativeQuery=true)
+    @Query( value = "SELECT * FROM move_history_new WHERE game_id= ?1", nativeQuery=true)
     Optional<List<MoveHistory>> fubdAllHistoryByGameId(String gameId);
 
 }
