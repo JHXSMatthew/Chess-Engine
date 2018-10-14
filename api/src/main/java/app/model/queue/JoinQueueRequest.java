f15b3160146af7422099263b0ce086b20045ea76
Gary Chen <cpldhycxc@gmail.com>
2018-10-15 01:30:16 +1100

    add the queue documentation

--- api/src/main/java/app/model/queue/JoinQueueRequest.java
+++ api/src/main/java/app/model/queue/JoinQueueRequest.java
@@ -2,15 +2,17 @@ package app.model.queue;
 
 import app.model.game.GameRoom;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by JHXSMatthew on 14/10/18.
  */
 public class JoinQueueRequest {
 
-
+    @ApiModelProperty(value = "current token")
     @JsonProperty("token")
     private String token;
+    @ApiModelProperty(value = "current game type")
     @JsonProperty("gameType")
     private GameRoom.GameType gameType;
 
